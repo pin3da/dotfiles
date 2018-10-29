b5c44affd1b0610f92eca4baa39db268177e1074
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2018-10-29 15:47:49 -0500

    update dotfiles

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,3 @@
-set -x TERM xterm-256color
 
 function mkcd
   mkdir $argv
@@ -6,12 +5,12 @@ function mkcd
 end
 
 function compi
-  g++ -static -Wall -O3 -DLOCAL $argv[1].cc -o $argv[1]
+  g++ -static -Wall -O3 -DLOCAL $argv[1].cc -o $argv[1].out
 end
 
-function cptest
-  eval time -p ./$argv[1] < $argv[2].in > $argv[1].out
-  if diff $argv[1].out $argv[2].ans
+function cprun
+  eval time -p ./$argv[1].out < $argv[2].in > $argv[1].tmp
+  if diff $argv[1].tmp $argv[2].ans
     set_color green
     echo 'passed'
     set_color normal
@@ -22,12 +21,31 @@ function cptest
   end
 end
 
+function cptest
+  compi $argv[1]
+  cprun $argv[1] $argv[2]
+end
+
 function cpbatch
   compi $argv[1]
   for i in (seq $argv[3] $argv[4])
-    cptest $argv[1] $argv[2]$i
+    cprun $argv[1] $argv[2]$i
   end
 end
 
+function pull
+  git pull --rebase origin master
+end
 
+set -gx TERM xterm-256color
+set -gx PATH $PATH /home/pin3da/Downloads/go/bin
+set -gx PATH $PATH /home/pin3da/n/bin/
 set -gx PATH  $PATH /home/pin3da/gopath/bin
+set -gx GOPATH /home/pin3da/gopath
+set -gx AIRFLOW_HOME /home/pin3da/airflow
+
+## Plugins
+
+fundle plugin 'tuvistavie/fish-ssh-agent'
+fundle init
+
--- link.sh
+++ link.sh
@@ -4,10 +4,10 @@ export REPO=`pwd`
 
 ln -fs $REPO/vimrc ~/.vimrc
 ln -fs $REPO/gitconfig ~/.gitconfig
-ln -fs $REPO/xbindkeysrc ~/.xbindkeysrc
 ln -fs $REPO/xmodmap ~/.xmodmap
 ln -fs $REPO/Xresources ~/.Xresources
 ln -fs $REPO/xsession ~/.xsession
+ln -fs $REPO/i3config ~/.i3/config
 cp -rf $REPO/config/* ~/.config
 
 rm -rf ~/.vim/plugin_settings
@@ -15,8 +15,3 @@ ln -fs $REPO/vim/plugin_settings ~/.vim/plugin_settings
 
 rm -rf ~/.vim/after
 ln -fs $REPO/vim/after ~/.vim/after
-
-
-if [ "`id -u`" = "0" ]; then
-  ln -fs $REPO/i3status.conf /etc/i3status.conf
-fi
