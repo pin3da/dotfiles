b48ef4830958c27da93088e113fd25305d8a075c
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2025-12-28 20:38:10 +0100

    Update bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+# Rofi seems broken in debian testing
+
 # All the packages I use in my sway config.
-sudo apt install sway rofi-wayland dunst spotify-client brightnessctl pulseaudio-utils playerctl kanshi wl-clipboard foot
+sudo apt install \
+	sway \
+	dunst \
+	spotify-client \
+	brightnessctl \
+	pulseaudio-utils \
+	playerctl \
+	kanshi \
+	wl-clipboard \
+	foot \
+	build-essential \
+	waybar \
+	btop \
+	curl 
