c3d5a987a664298ce10556e545df0adf16e11931
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2025-12-28 21:29:44 +0100

    Use wofi as launcher:
    rofi is broken in debian testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,41 +3,49 @@
 # NOTE: Rofi seems broken in debian testing, should be added later
 
 install_packages() {
-    echo "Installing required packages..."
-    sudo apt update || { echo "Failed to update package lists. Exiting."; exit 1; }
+  echo "Installing required packages..."
+  sudo apt update || {
+    echo "Failed to update package lists. Exiting."
+    exit 1
+  }
 
-    sudo apt install -y \
-        sway \
-        dunst \
-        spotify-client \
-        brightnessctl \
-        pulseaudio-utils \
-        playerctl \
-        kanshi \
-        wl-clipboard \
-        foot \
-        build-essential \
-        waybar \
-        btop \
-        curl \
-        pipewire || { echo "Package installation failed. Exiting."; exit 1; }
+  sudo apt install -y \
+    sway \
+    dunst \
+    spotify-client \
+    brightnessctl \
+    pulseaudio-utils \
+    playerctl \
+    kanshi \
+    wl-clipboard \
+    foot \
+    build-essential \
+    waybar \
+    btop \
+    curl \
+    neovim \
+    clang libclang-dev \
+    wofi \
+    pipewire || {
+    echo "Package installation failed. Exiting."
+    exit 1
+  }
 }
 
 restart_services() {
-    echo "Remember to restart the services if needed:"
-      
-    echo "  sudo systemctl --user restart wireplumber pipewire pipewire-pulse"
+  echo "Remember to restart the services if needed:"
+
+  echo "  sudo systemctl --user restart wireplumber pipewire pipewire-pulse"
 }
 
-
 main() {
-    # Install packages
-    install_packages
+  # Install packages
+  install_packages
 
-    # Restart necessary services
-    restart_services
+  # Restart necessary services
+  restart_services
 
-    echo "Script completed successfully!"
+  echo "Script completed successfully!"
 }
 
 main
