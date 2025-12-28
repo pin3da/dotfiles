d7ed5d832a663591e0fac85617b2685927d5782b
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2025-12-28 21:04:49 +0100

    Improve bootstrap script a bit.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-# Rofi seems broken in debian testing
+# NOTE: Rofi seems broken in debian testing, should be added later
 
-# All the packages I use in my sway config.
-sudo apt install \
-	sway \
-	dunst \
-	spotify-client \
-	brightnessctl \
-	pulseaudio-utils \
-	playerctl \
-	kanshi \
-	wl-clipboard \
-	foot \
-	build-essential \
-	waybar \
-	btop \
-	curl 
+install_packages() {
+    echo "Installing required packages..."
+    sudo apt update || { echo "Failed to update package lists. Exiting."; exit 1; }
+
+    sudo apt install -y \
+        sway \
+        dunst \
+        spotify-client \
+        brightnessctl \
+        pulseaudio-utils \
+        playerctl \
+        kanshi \
+        wl-clipboard \
+        foot \
+        build-essential \
+        waybar \
+        btop \
+        curl \
+        pipewire || { echo "Package installation failed. Exiting."; exit 1; }
+}
+
+restart_services() {
+    echo "Remember to restart the services if needed:"
+      
+    echo "  sudo systemctl --user restart wireplumber pipewire pipewire-pulse"
+}
+
+
+main() {
+    # Install packages
+    install_packages
+
+    # Restart necessary services
+    restart_services
+
+    echo "Script completed successfully!"
+}
+
+main
