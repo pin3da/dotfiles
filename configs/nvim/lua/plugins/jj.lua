8f4fac0e9ecd9ef02f867d743cd50d0b0c65cf34
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2026-04-13 13:48:12 +0200

    Add plugin for jj description buffers in Neovim.

---
+++ configs/nvim/lua/plugins/jj.lua
@@ -0,0 +1,5 @@
+return {
+	-- Syntax highlighting for jj description buffers (*.jjdescription).
+	-- Handles "JJ:" comment prefix correctly, unlike the gitcommit filetype.
+	{ "avm99963/vim-jjdescription" },
+}
